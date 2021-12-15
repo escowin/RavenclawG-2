ee0dc10b109d00ba330d7f70edf642d9e4d75293
yohans14 <yohans14@yahoo.com>
2021-12-14 18:45:32 -0600

    fetched new api

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,1 +1,14 @@
 // #feature branch news 
+fetch("https://google-news.p.rapidapi.com/v1/top_headlines?lang=en&country=US", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "google-news.p.rapidapi.com",
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
