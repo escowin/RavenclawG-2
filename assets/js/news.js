576ef263ce75c33ac09bc5766d9027c2bcc26ea2
yohans14 <yohans14@yahoo.com>
2021-12-14 19:51:12 -0600

    new fetch

--- assets/js/news.js
+++ assets/js/news.js
@@ -3,7 +3,7 @@ fetch("https://google-news.p.rapidapi.com/v1/top_headlines?lang=en&country=US", 
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "google-news.p.rapidapi.com",
-		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+		"x-rapidapi-key": "74fc7b6b2b8d4e368517bcefe10f1d07"
 	}
 })
 .then(response => {
