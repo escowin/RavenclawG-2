649773f3f9933c6c64783cb07fd668fc6b6a56b2
Nick Garza <thenickgarza14@gmail.com>
2021-12-16 09:57:06 -0600

    have the search working to search for a specfic player

--- assets/js/sports.js
+++ assets/js/sports.js
@@ -1,7 +1,7 @@
 // gets all the players
-var apiUrl = "https://www.balldontlie.io/api/v1/players?search=" 
+// var apiUrl = "https://www.balldontlie.io/api/v1/players?search=" 
 var searchEl = document.querySelector('#searchNBA');
-var buttonEl = document.querySelector('#update-btn');
+var buttonEl = document.querySelector('#nba-btn');
 var dataEl = document.querySelector('#nbaPlayer');
 
 
@@ -9,25 +9,26 @@ var playerSumbitHandler = function(event) {
     // prevent page from refreshing 
     event.preventDefault();
     // get the value from the input
-    var playerSearch = searchEl.value;
+    var playerSearch = searchEl.value.trim();
 
     if(playerSearch) {
         getPlayer(playerSearch);
         // clear the old content
+        searchEl.value = '';
     } else {
         alert('Please enter a player');
     }
 };
 
-var getPlayer = function() {
+var getPlayer = function(playerSearch) {
     // format the Api
-    var apiUrl = "https://www.balldontlie.io/api/v1/players?search=" + playerSearch 
+    var apiUrl = "https://www.balldontlie.io/api/v1/players?search=" + playerSearch
     // fetch the api
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => console.log(data));
+    
 
-}
+};
 
-
-buttonEl.addEventListener("update", playerSumbitHandler)
+buttonEl.addEventListener("click", playerSumbitHandler)
