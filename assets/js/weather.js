ce3d09824ee4545c305775e89890e77382915604
cdtest <corydavis1989@gmail.com>
2021-12-17 12:02:24 -0600

    All data is displaying/Basic functionality is there

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,10 @@
 let cityInputForm = document.querySelector(".citySearchForm");
 let cityInputField = document.querySelector(".citySearchInput");
-
-
-
+let citySearched = document.querySelector(".w-citySearch");
+let weatherDescription = "";
+let currentTemp = document.querySelector(".w-temp");
+let iconDisplay = document.querySelector(".w-icons");
+let Humidity = document.querySelector(".w-moreInfo");
 
 
 // Gathers api data from this endpoint
@@ -10,7 +12,7 @@ var getOpenWeatherData = function(city) {
     var endPoint = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=9cea4c2c630ad1c978d7cdfabc1cee75";
     fetch(endPoint).then(function(response) {
     response.json().then(function(data) {
-        console.log(data);
+        displayWeatherData(data);
     });
   })
 };
@@ -29,3 +31,18 @@ var citySubmitHandler = function(event) {
 
 // Submit Event for the City Search Form
   cityInputForm.addEventListener("submit", citySubmitHandler);
+
+
+  // Displays data on the page
+  var displayWeatherData = function(data) {
+    console.log(data);
+
+    citySearched.textContent = data.name;
+    weatherDescription.textContent = data.weather[0].description;
+    currentTemp.textContent = Math.round(data.main.temp) + " °F";
+    Humidity.textContent = "Humidity: " + data.main.humidity + "%";
+
+    let weatherIcon = data.weather[0].icon;
+    iconDisplay.textContent = weatherIcon;
+    iconDisplay.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png");
+  };
