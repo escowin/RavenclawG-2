e442d97471d5620f9b406b0a31bafa8ab1697854
escowin <edwin@escowinart.com>
2022-04-24 12:21:05 -0500

    html:196-203 stock search bar

--- assets/js/stocks.js
+++ assets/js/stocks.js
@@ -1,13 +1,14 @@
 // Alpha Vantage API
 // cost apiUrl = "https://www.alphavatage.co/query?function=";
-// const apiKey = "apiKey=0FNPYTR1466MR51T";
+const apiKey = "apiKey=0FNPYTR1466MR51T";
 // const intraDay = "function=TIME_SERIES_INTRADAY";
 // const 
 
 var getStocks = function() {
-    console.log("getStocks is doing something");
+    fetch("https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=EUR&to_symbol=USD&interval=5min&apikey=demo");
   };
-  
+  console.log(getStocks);
+
 
   getStocks();
 
