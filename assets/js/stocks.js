84fb32abee58d22c14a4a37e21638f8f44cabc8b
escowin <edwin@escowinart.com>
2022-04-24 01:53:39 -0500

    commented out old code, using alpha vantage api for stocks.js

---
+++ assets/js/stocks.js
@@ -0,0 +1,27 @@
+// Alpha Vantage API
+// API Key 0FNPYTR1466MR51T
+
+// TIME_SERIES_INTRADAY | 
+
+//  Finance API widget stuff
+
+// var axios = require("axios").default;
+
+
+
+
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://yfapi.net/v11/finance/quoteSummary/AAPL',
+//   params: {modules: 'defaultKeyStatistics,assetProfile'},
+//   headers: {
+//     'x-api-key': 'RDFVnMFH1btOrenf8xIH8WPlt2dgqCk5i1hNIa68'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
