cecfe146bc4aea900d0a21f9ada508ab603392ab
escowin <edwin@escowinart.com>
2022-04-25 08:18:32 -0500

    bug | full url (with apikey) accessible in browser, but js:17 c.log() as error messagge 'paramater apikey is invalid or missing'

--- assets/js/stocks.js
+++ assets/js/stocks.js
@@ -6,18 +6,25 @@ var stockHighEl = document.querySelector("#stock-high");
 var stockLowEl = document.querySelector("#stock-low");
 
 // STOCKS | Alpha Vantage API
+var urlBase = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=";
 var apiKey = "apiKey=0FNPYTR1466MR51T";
 
 // RETRIEVE API STOCK DATA
-var getStock = function (stock) {
-    var endPoint = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=5min&" + apiKey;
-    fetch(endPoint)
-    .then(response => response.json())
-    .then(data => {
-        stockNameEl.innerHTML = "Stock | " + data.data[0].symbol;
-        stockHighEl.innerHTML = "High | " + data.data[0].high;
-        weightEl.innerHTML = "Low | " + data.data[0].low;
-    })
+var getStock = function () {
+    // var endPoint = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=5min&" + apiKey;
+    var apiUrl = fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&" + apiKey).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+    // fetch(endPoint)
+    // .then(response => response.json())
+    // .then(data => {
+    //     stockNameEl.innerHTML = "Stock | " + data.data[0].symbol;
+    //     stockHighEl.innerHTML = "High | " + data.data[0].high;
+    //     weightEl.innerHTML = "Low | " + data.data[0].low;
+    // })
 };
 
 // USER INPUT
@@ -35,6 +42,7 @@ var stockSubmitHandler = function (event) {
 
 // DISPLAY STOCK DATA ON HOMEPAGE
 var displayStock = function (data) {
+    console.log(data);
     stockSearched.innerHTML = data.name;
     stockMain.innerHTML.setItem("current-stock", data.stock[0].main);
     localStorage.setItem("current-stock")
